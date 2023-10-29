1d2d6cdc2b440165ac0ea96de0d9c08df7d4ce7c
DeluxPanda <136261250+DeluxPanda@users.noreply.github.com>
2023-10-29 10:33:39 +0100

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -35,7 +35,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    { "[]=",      tile },    /* first entry is the default */
+    { "",      tile },    /* first entry is the default */
     { "><>",      NULL },    /* no layout function means floating behavior */
     { "[M]",      monocle },
 };
