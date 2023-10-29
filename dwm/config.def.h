93f8914fe37bed51f47eb29d52264b72726b5493
DeluxPanda <136261250+DeluxPanda@users.noreply.github.com>
2023-10-29 02:50:12 +0100

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"JetBrains Mono:size=11", "JoyPixels:pixelsize=11:antialias=true:autohint=true"};;
+static const char *fonts[]          = {"JetBrains Mono:size=11", "JoyPixels:pixelsize=11:antialias=true:autohint=true"};
 static const char dmenufont[]       = "JetBrains Mono:size=11";
 static const char col_gray1[]       = "#2C2C2C"; //Black
 static const char col_gray2[]       = "#2C2C2C"; //Black
