ee77ba5788e0617566f124072e279fe233614c0a
DeluxPanda <136261250+DeluxPanda@users.noreply.github.com>
2023-11-05 17:27:39 +0100

    Update config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -28,7 +28,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Steam",    NULL,       NULL,       1 << 6,          0,         -1 },
+	{ "Steam",    NULL,       NULL,       1 << 5,          0,         -1 },
 
 };
 
