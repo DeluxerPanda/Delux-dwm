a9daf37442582ab9312838e8d9eb7a3bb8076b46
DeluxerPanda <cedrikpersson@gmail.com>
2024-03-11 21:11:54 +0100

    update. all monitors have statusbar

--- suckless/slstatus/config.h
+++ suckless/slstatus/config.h
@@ -64,6 +64,12 @@ static const char unknown_str[] = "n/a";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+        {disk_free, "| %s", "/"},
+        {disk_total, " / %s", "/"},
+//      { run_command, " | :%4s","amixer sget Master | awk -F\"[][]>
+        { cpu_perc, " | : %s%%", NULL},
+        { ram_perc, " | : %s%%", NULL},
+//      { battery_perc, " |  %s%%","BAT0"},
+        { datetime, " | %s","%F | %T |"},
 };
+
