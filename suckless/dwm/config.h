c6b846f7fc35a0abd5d126be8518e8eddbafe656
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2024-03-20 20:33:34 +0100

    update

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -2,14 +2,14 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 26;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 1;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;     /* 0 means no bar */
-static const int topbar             = 1;     /* 0 means bottom bar */
+static const int showsystray        = 1;        /* 0 means no systray */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 
 static const char *fonts[]          = { "JetBrainsMonoNL Nerd Font Mono:size=15", "JoyPixels:pixelsize=15:antialias=true:autohint=true" };
 static const char dmenufont[]       = "JetBrainsMonoNL Nerd Font Mono:size=15";
