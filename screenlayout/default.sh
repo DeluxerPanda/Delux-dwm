5ec281df6e7290460ed838042146ec1e839711f2
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2025-03-02 16:07:54 +0100

    mini fix

--- screenlayout/default.sh
+++ screenlayout/default.sh
@@ -3,7 +3,7 @@
 
 if  xrandr | grep -q "HDMI-A-1 connected"; then
 
-xrandr --output DisplayPort-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-A-0 --mode 1366x768 --pos 0x321 --rotate normal --output HDMI-A-1 --mode 1920x1080 --pos 1366x0 --rotate normal
+xrandr --output DisplayPort-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-A-0 --mode 1366x768 --pos 0x156 --rotate normal --output HDMI-A-1 --mode 1920x1080 --pos 1366x0 --rotate normal
 
 else
 
