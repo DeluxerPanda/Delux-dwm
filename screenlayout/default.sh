6255e44e6036259194863831951ed8e0d7b6a245
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2025-03-02 16:04:52 +0100

    fix Screen layout script :/

--- screenlayout/default.sh
+++ screenlayout/default.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-if  xrandr | grep -q "DisplayPort-0 connected"; then
+if  xrandr | grep -q "HDMI-A-1 connected"; then
 
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-A-0 --mode 1366x768 --pos 0x321 --rotate normal --output HDMI-A-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-A-1-2 --off
+xrandr --output DisplayPort-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-A-0 --mode 1366x768 --pos 0x321 --rotate normal --output HDMI-A-1 --mode 1920x1080 --pos 1366x0 --rotate normal
 
 else
 
- xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-A-0 --mode 1366x768 --pos 0x156 --rotate normal --output HDMI-A-1 --off --output HDMI-A-1-2 --off
+ xrandr --output DisplayPort-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-A-0 --mode 1366x768 --pos 0x156 --rotate normal --output HDMI-A-1 --off
 fi
 
 feh --randomize --bg-fill ~/Bilder/backgrounds/*
