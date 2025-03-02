7473712a032fdb0823d3973ba403a2f4e434796b
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2025-03-02 01:12:46 +0100

    update

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
-# Install
-## Clone the repo
+# ⚠️ **VARNING!**  
+
+<span style="color:red;">Detta script körs på egen risk!</span>  
+Vi tar inget ansvar för eventuella skador eller dataförluster.  
+
+> **Viktigt:** Se till att du förstår konsekvenserna innan du fortsätter.
+
+#
+
+# Installera
+# klona repot
 ```
 git clone https://github.com/DeluxerPanda/Delux-dwm.git
 ```
-## Go to the cloned repo
+# Gå till det klonade repot
 ```
 cd Delux-dwm
 ```
-## Make the setup file executable
+# Gör installationsskriptet körbart
 ```
 chmod +x setup_for_Arch.sh
 ```
-## Run the setup script
+# Kör installationsskriptet
 ```
 ./setup_for_Arch.sh
-```
-
-## one liner
-```
-git clone https://github.com/DeluxerPanda/Delux-dwm.git && cd Delux-dwm && chmod +x setup_for_Arch.sh && ./setup_for_Arch.sh
 ```
--- setup_for_Arch.sh
+++ setup_for_Arch.sh
@@ -11,41 +11,29 @@ separator="---------------------------------------------------------------------
 
 
 if [ "$(id -u)" -eq 0 ]; then
+    printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$titel_message" ""
 
+    message="You can't run as root"
+    message_length=${#message}
+    spaces=$(( (${#separator} - message_length) / 2 ))
 
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$titel_message" ""
+    printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
 
-message="You can't run as root"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-
-exit 1
+    exit 1
 else
 
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$titel_message" ""
+    printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$titel_message" ""
 
-message="Script starting"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-echo -ne "
- 1. Install AUR helper and rate mirrors.
- 2. Update the system.
- 3. Install dependencies.
- 4. Ask the user if they want kitty, dolphin, fastfetch, qt6ct, kvantum, rofi, firefox, ark, btop, pavucontrol
- 5. Install Picom animations.
- 6. Install DWM and Slstatus.
- 7. Ask the user if they want to move 20-amdgpu.conf to /etc/X11/xorg.conf.d/.
-"
-printf "%s\n%${spaces}s%s\n%s\n" "$separator"
+    message="Script starting"
+    message_length=${#message}
+    spaces=$(( (${#separator} - message_length) / 2 ))
+    printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
 
 while true; do
 
-read -p "Do you want to run this script? (y/n) " yn
+    read -p "Do you want to run this script? (y/n) " yn
 
-case $yn in
+    case $yn in
 	[yY] | [jJ] | [sS][oO] | [yY][eE][sS] | [jJ][aA] )
 		echo "ok, we will proceed"
 		break
@@ -57,244 +45,144 @@ case $yn in
 	* )
 		echo "invalid response"
 		;;
-esac
-
+    esac
+    
 done
-
-fi
-
-
-
-if [ -z "$work_dir" ]; then
-    work_dir=$(pwd)
-fi
-
-#Installing: AUR helper and Rate mirrors
-message="Installing: AUR helper and Rate mirrors"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-
-command_exists() {
-    which $1 >/dev/null 2>&1
-}
-RC='\033[0m'
-RED='\033[31m'
-
-            if ! command_exists yay && ! command_exists paru; then
-                echo "Installing yay as AUR helper..."
-                sudo pacman --noconfirm -S base-devel
-                cd /opt && sudo git clone https://aur.archlinux.org/yay-git.git && sudo chown -R "$USER":"$USER" ./yay-git
-                cd yay-git && makepkg --noconfirm -si
-            else
-                echo "Aur helper already installed"
-            fi
-            if command_exists yay; then
-                AUR_HELPER="yay"
-            elif command_exists paru; then
-                AUR_HELPER="paru"
-            else
-                echo "No AUR helper found. Please install yay or paru."
-                exit 1
-            fi
-
-      if ! command_exists rate-mirrors; then
-      ${AUR_HELPER} --noconfirm -S rate-mirrors-bin
-        fi
-      if [ -s /etc/pacman.d/mirrorlist ]; then
-          sudo cp -r /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-      fi
-      echo "checking out mirrors..."
-      sudo rate-mirrors --top-mirrors-number-to-retest=5 --disable-comments --save /etc/pacman.d/mirrorlist --allow-root arch
-
-#Updating system
-message="Updating system"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-
-sudo pacman -Syu --noconfirm
-
-#Installing dependencies
-message="Installing dependencies"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-
-
-sudo pacman -S --noconfirm base-devel libx11 libxinerama libxft imlib2 hicolor-icon-theme libconfig dbus libepoxy libev libglvnd libxcb libxext pcre2 pixman xcb-util-image xcb-util-renderutil asciidoc mesa meson uthash xorgproto python xorg-xprop xorg-xwininfo 
-
-
-message="Installing kitty, dolphin, fastfetch, qt6ct, kvantum rofi, firefox, ark, btop, pavucontrol"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-
-while true; do
-
-read -p "Do you want to install? (y/n) " yn
-
-case $yn in
-	[yY] | [jJ] | [sS][oO] | [yY][eE][sS] | [jJ][aA] )
-		echo "ok, we will proceed"
-		sudo pacman -S --noconfirm kitty dolphin fastfetch qt6ct kvantum rofi firefox ark btop pavucontrol
-		break
-		;;
-	[nN] | [nN][oO] | [nN][eE][iI][nN] )
-		echo "Oki"
-		break
-		;;
-	* )
-		echo "invalid response"
-		;;
-esac
-
-done
-
-echo 'QT_QPA_PLATFORMTHEME=qt6ct' | sudo tee -a /etc/environment
-
-#Installing picom animations
-message="Installing picom animations"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
-
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-
-    # Clone the repository in the home/build directory
-    mkdir -p ~/build
-    if [ ! -d ~/build/picom ]; then
-        if ! git clone https://github.com/FT-Labs/picom.git ~/build/picom; then
-            echo "Failed to clone the repository"
-            return 1
-        fi
-    else
-        echo "Repository already exists, skipping clone"
+    
+    fi
+    
+    if [ -z "$work_dir" ]; then
+        work_dir=$(pwd)
     fi
 
-    cd ~/build/picom || { echo "Failed to change directory to picom"; return 1; }
+    # Enabling multilib repository for Steam
+    message="Enabling multilib repository for Steam"
+    message_length=${#message}
+    spaces=$(( (${#separator} - message_length) / 2 ))
 
-    # Build the project
-    if ! meson setup --buildtype=release build; then
-        echo "Meson setup failed"
-        return 1
-    fi
+    printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
 
-    if ! ninja -C build; then
-        echo "Ninja build failed"
-        return 1
-    fi
+    sudo sed -i '/#\[multilib\]/s/^#//' /etc/pacman.conf
+    sudo sed -i '/#Include = \/etc\/pacman.d\/mirrorlist/s/^#//' /etc/pacman.conf
 
-    # Install the built binary
-    if ! sudo ninja -C build install; then
-        echo "Failed to install the built binary"
-        return 1
-    fi
+    #Updating system
+    message="Updating system"
+    message_length=${#message}
+    spaces=$(( (${#separator} - message_length) / 2 ))
+    
+    printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
+    
+    sudo pacman -Syy --noconfirm
+    
+    sudo pacman -S --noconfirm base-devel git libx11 libxft xorg-server xorg-xinit bash-completion ttf-jetbrains-mono-nerd noto-fonts-emoji bat
+    
+    sudo pacman -S --noconfirm fastfetch rofi btop picom starship feh ffmpeg pcmanfm gnome-keyring arandr steam flatpak
+    
+    flatpak install -y flathub org.prismlauncher.PrismLauncher
 
-    echo "Picom animations installed successfully"
+    flatpak install -y flathub com.chatterino.chatterino
 
-#Installing starship
-message="Installing starship"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
+    message="Installing starship"
+    message_length=${#message}
+    spaces=$(( (${#separator} - message_length) / 2 ))
+    
+    printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
 
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-
-if [ -s ~/.config/starship.toml ]; then
-     cp -r ~/.config/starship.toml ~/.config/starship.toml.bak
-     else
-    curl -sS https://starship.rs/install.sh | sh -s -- --yes
-    echo "starship installed successfully"
-fi
-     cp -r $work_dir/configs/starship.toml ~/.config/starship.toml
+     cp -r $work_dir/config/starship.toml ~/.config/starship.toml
     echo "Moving starship.toml"
 
-
-
-
     echo "Copy Rofi config files"
-    if [ -d ~/.config/rofi/ ]; then
-        cp -r ~/.config/rofi ~/.config/rofi.bak
-    fi
-    mkdir -p ~/.config/rofi
-    cp -r $work_dir/configs/rofi/powermenu.sh ~/.config/rofi/powermenu.sh
-    chmod +x ~/.config/rofi/powermenu.sh
-    cp -r $work_dir/configs/rofi/config.rasi ~/.config/rofi/config.rasi
-    mkdir -p ~/.config/rofi/themes
-    cp -r $work_dir/configs/rofi/themes/catppuccin-mocha.rasi ~/.config/rofi/themes/catppuccin-mocha.rasi
-    cp -r $work_dir/configs/rofi/themes/powermenu.rasi ~/.config/rofi/themes/powermenu.rasi
 
+    mkdir -p ~/.config/rofi
+    cp -r $work_dir/config/rofi/ ~/.config/rofi/
 
     echo "Copy fastfetch config file"
-    if [ -d ~/.config/fastfetch/ ]; then
-        cp -r ~/.config/fastfetch ~/.config/fastfetch.bak
-    fi
-    cp -r $work_dir/configs/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
-    cp -r $work_dir/configs/fastfetch/logo.png ~/.config/fastfetch/logo.png
+
+    cp -r $work_dir/config/fastfetch/ ~/.config/fastfetch/
 
 
 
-#Installing dwm, slstatus
-message="Installing dwm, slstatus"
-message_length=${#message}
-spaces=$(( (${#separator} - message_length) / 2 ))
+    #Installing dwm, slstatus
+    message="Installing dwm, st, slstatus"
+    message_length=${#message}
+    spaces=$(( (${#separator} - message_length) / 2 ))
 
-printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
+    printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
 
-for app in "dwm" "slstatus"; do
-
-    echo "Going to $app"
-    cd $work_dir/"$app"
-
-    echo "Making $app"
+    rm -rf dwm
+    git clone https://github.com/DeluxerPanda/dwm.git
+    cd dwm
     sudo make clean install
+    cd $work_dir
 
-done
+    rm -rf st
+    git clone https://github.com/DeluxerPanda/st.git
+    cd st
+    sudo make clean install
+    cd $work_dir
 
-for app in ".bashrc" ".bash_profile"; do
-  if [ -s ~/$app ]; then
-    cp -r ~/$app ~/$app.bak
- fi
-     echo "Moving $app"
-    cp -r $work_dir/configs/"$app" ~/
-done
+    rm -rf slstatus
+    git clone https://github.com/DeluxerPanda/slstatus.git
+    cd slstatus
+    sudo make clean install
+    cd $work_dir
 
-#moving fonts
 
-sudo mkdir -p /usr/share/fonts/TTF
-sudo cp -r $work_dir/Fonts/JetBrainsMonoNLNerdFontMono-Regular.ttf /usr/share/fonts/TTF/
-fc-cache
+    for app in ".bashrc" ".bash_profile"; do
+         echo "Moving $app"
+         cp -r $work_dir/config/"$app" ~/
+    done
 
-cd $work_dir
+		sudo cp -r $work_dir/config/20-amdgpu.conf /etc/X11/xorg.conf.d/20-amdgpu.conf
 
-while true; do
+        # Get the current username
+        USERNAME=$(logname)
 
-read -p "Do you want to move 20-amdgpu.conf to /etc/X11/xorg.conf.d/ (y/n) " yn
+        # Check if user exists
+        if ! id "$USERNAME" &>/dev/null; then
+            echo "User '$USERNAME' does not exist. Please create the user first."
+            exit 1
+        fi
 
-case $yn in
-	[yY] | [jJ] | [sS][oO] | [yY][eE][sS] | [jJ][aA] )
-		echo "Moving 20-amdgpu.conf to /etc/X11/xorg.conf.d/"
-		sudo cp -r $work_dir/configs/20-amdgpu.conf /etc/X11/xorg.conf.d/20-amdgpu.conf
-		break
-		;;
-	[nN] | [nN][oO] | [nN][eE][iI][nN] )
-		message="The script is done"
+        # Create systemd override directory if not exists
+        sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+
+        # Write the override configuration
+        sudo bash -c 'cat <<EOF > /etc/systemd/system/getty@tty1.service.d/override.conf
+        [Service]
+        ExecStart=
+        ExecStart=-/usr/bin/agetty --autologin $USERNAME --noclear %I \$TERM 
+        EOF'
+
+        # Reload systemd and restart getty service
+        sudo systemctl daemon-reexec
+        sudo systemctl restart getty@tty1
+
+        # Enable the service at boot
+        sudo systemctl enable getty@tty1
+
+
+
+		message="Setting up systemd service and timer för Flatpak"
         message_length=${#message}
         spaces=$(( (${#separator} - message_length) / 2 ))
         printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
-		exit
-		;;
-	* )
-		echo "invalid response"
-		;;
-esac
 
-done
 
-		message="The script is done"
+        echo "Updating Flatpak applications..."
+        flatpak update -y
+
+        # Setup systemd service and timer
+        echo "Setting up systemd service and timer..."
+
+        sudo cp -r $work_dir/services/flatpak-update.service /etc/systemd/system/flatpak-update.service
+
+        sudo systemctl daemon-reload
+        sudo systemctl enable --now flatpak-update.timer
+        echo "Systemd service and timer set up successfully!"
+
+
+		message="Klart"
         message_length=${#message}
         spaces=$(( (${#separator} - message_length) / 2 ))
         printf "%s\n%${spaces}s%s\n%s\n" "$separator" "" "$message" "$separator"
