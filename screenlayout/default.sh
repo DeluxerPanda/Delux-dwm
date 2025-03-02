0085b50ec45fd6f9c7dd75fca350301073cfb64f
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2025-03-02 15:54:25 +0100

    fix Screen layout script

--- screenlayout/default.sh
+++ screenlayout/default.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if xrandr | grep -q "DisplayPort-1"; then
+
+if  xrandr | grep -q "DisplayPort-0 connected"; then
 
 xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-A-0 --mode 1366x768 --pos 0x321 --rotate normal --output HDMI-A-1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI-A-1-2 --off
 
